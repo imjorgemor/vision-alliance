8488f0aa1c8c07fbb0b8ac94aad3b39dd199a530
Jorge Mor <imgeorgemore@icloud.com>
2022-10-28 20:25:59 +0200

    Update contact info

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,10 +1,9 @@
 import AppWrap from '../wrapper/AppWrap';
 import Triangle from "../components/Triangle"
-import WilsonLogo from "../assets/wmlogo.png"
 import HuxleyLogo from "../assets/huxleylogo.png"
 import "../styles/Contact.scss"
 
-const PARTNER_LOGO = [WilsonLogo, HuxleyLogo]
+const PARTNER_LOGO = [HuxleyLogo]
 
 
 const Contact = () => {
@@ -25,7 +24,6 @@ const Contact = () => {
                     <a href="https://mnlaborershealthwellnessclinics.org/your-health-benefits/health-fair/" className='link' target="_blank" rel="noreferrer"><span>Link here</span></a>
                 </div>
                 <div>
-                    <h3 className='contacts-label mt-1'>Zenith MN office:</h3>
                     <a href="tel:(651) 403 0553" className='link'><span><span><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="white" d="M6.176 1.322l2.844-1.322 4.041 7.89-2.724 1.341c-.538 1.259 2.159 6.289 3.297 6.372.09-.058 2.671-1.328 2.671-1.328l4.11 7.932s-2.764 1.354-2.854 1.396c-7.862 3.591-19.103-18.258-11.385-22.281zm1.929 1.274l-1.023.504c-5.294 2.762 4.177 21.185 9.648 18.686l.971-.474-2.271-4.383-1.026.5c-3.163 1.547-8.262-8.219-5.055-9.938l1.007-.497-2.251-4.398z" /></svg></span>(651) 403-0533</span></a>
                 </div>
                 <div>
